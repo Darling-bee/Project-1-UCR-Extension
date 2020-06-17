7bf12cba394d53edb52407fa622786259e6f1cd9
imartinez0753 <50923315+imartinez0753@users.noreply.github.com>
2020-06-16 21:26:45 -0700

    I'm a dummy here ya go

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
 $(document).ready(function () {
 	console.log("ready!");
 
+	$.ajax({
+		url: "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+		method: "GET",
+	}).then(function (response) {
+		console.log(response);
+	});
+
 	//we need five buttons for spirits
 	//each button would need an id that is the href
-	$(".button").on("click", function () {
-		document.location.href = $(this).attr("id");
+	// $("button").on("click", function () {
+	// 	document.location.href = $(this).attr("id");
+	// });
+	var vodkaList = $("<ul>");
+	$("#vodka").append(vodkaList);
+	var whiskeyList = $("<ul>");
+	$("#whiskey").append();
+
+	$.ajax({
+		url: "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+		method: "GET",
+	}).then(function (response) {
+		console.log(response);
+
+		$("button").on("click", function () {
+			for (var i = 0; response.drinks.length; i++) {
+				var drinkList = $("<li>");
+				drinkList.text(response.drinks[i].strDrink);
+				$(vodkaList).append(drinkList);
+			}
+		});
 	});
-	$(".button").on("click", function () {
-		document.location.href = $(this).attr("id");
-	});
-	$(".button").on("click", function () {
-		document.location.href = $(this).attr("id");
-	});
-	$(".button").on("click", function () {
-		document.location.href = $(this).attr("id");
-	});
-	$(".button").on("click", function () {
-		document.location.href = $(this).attr("id");
-	});
+	// $("button").on("click", function () {
+	// 	document.location.href = $(this).attr("id");
+	// });
+	// $("button").on("click", function () {
+	// 	document.location.href = $(this).attr("id");
+	// });
+	// $("button").on("click", function () {
+	// 	document.location.href = $(this).attr("id");
+	// });
 });
