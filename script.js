22322dc2dd5bdea57889e74e9792a5c11122ab68
Yulin Karaiscos <63887408+KarazKOS@users.noreply.github.com>
2020-06-23 17:37:22 -0700

    Second wave of files that were edited.
    Includes HTML, CSS, Javascript and images

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// Carousel "Full Width" function
-// var instance = M.Carousel.init({
-//   fullWidth: true,
-// });
+M.AutoInit();
 
-// Or with jQuery
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
 
-// $(".carousel.carousel-slider").carousel({
-//   fullWidth: true,
-// });
-
-// ----------------------------
-
-// Materialize auto initialize function: Auto Init allows you to initialize all of the Materialize Components with a single function call. It is important to note that you cannot pass in options using this method.
-
-// M.AutoInit();
+var instance = M.Carousel.init({
+  fullWidth: true,
+});
